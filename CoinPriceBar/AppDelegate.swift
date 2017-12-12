7ef6b7c442b80aa00a5bd7a5849dcea761e52a54
Thanh Pham <minhthanh@me.com>
2017-12-12 13:25:41 +0800

    Initial Commit

---
+++ CoinPriceBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CoinPriceBar
+//
+//  Created by Thanh Pham on 12/12/17.
+//  Copyright © 2017 Thanh Pham. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
