79d15ba6e591a3fc387ad42e50c729a18bf611fd
Thanh Pham <minhthanh@me.com>
2017-12-13 22:31:45 +0800

    add system touch bar item

--- CoinPriceBar/AppDelegate.swift
+++ CoinPriceBar/AppDelegate.swift
@@ -11,6 +11,7 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
   let window: NSWindow
+  let btcCharacter = "₿"
 
   let coinPriceTouchBarController: CoinPriceTouchBarController = {
     let coinbasePriceProvider = CoinbasePriceProvider()
@@ -24,7 +25,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   }
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
+    DFRSystemModalShowsCloseBoxWhenFrontMost(true)
+    let customTouchBarItem = NSCustomTouchBarItem(identifier: NSTouchBarItem.Identifier(rawValue: btcCharacter))
+    let customTouchBarItemButton = NSButton(title: btcCharacter, target: self, action: #selector(AppDelegate.customTouchBarItemTapped))
+    customTouchBarItem.view = customTouchBarItemButton
+    NSTouchBarItem.addSystemTrayItem(customTouchBarItem)
+    DFRElementSetControlStripPresenceForIdentifier(customTouchBarItem.identifier.rawValue, true)
+
     window.touchBar = coinPriceTouchBarController.touchBar
     window.makeKeyAndOrderFront(nil)
+  }
+
+  @objc func customTouchBarItemTapped() {
+    NSTouchBar.presentSystemModalFunctionBar(coinPriceTouchBarController.touchBar, systemTrayItemIdentifier: btcCharacter)
   }
 }
