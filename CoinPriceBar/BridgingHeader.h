2b120ffae4ecb12cffee98472c5bfe8f5dab853f
Thanh Pham <minhthanh@me.com>
2017-12-13 09:44:21 +0800

    link private APIs

---
+++ CoinPriceBar/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  CoinPriceBar
+//
+//  Created by Thanh Pham on 13/12/17.
+//  Copyright © 2017 Thanh Pham. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "TouchBar.h"
+
+#endif /* BridgingHeader_h */
